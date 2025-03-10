4d455c36dcbf481b7143835f5a50b711db8fc16b
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-03-10 12:57:35 +0100

    Commit Messagr

--- src/pages/LineAnimation.tsx
+++ src/pages/LineAnimation.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import SplitType from "split-type";
 
 const LineAnimation = () => {
-  return (
-    <div className="flex items-center justify-center h-[100dvh] w-full">
-        <h1 className="text-4xl font-bold">Advanced Learning with AI</h1>
-    </div>
-  )
-}
+  const textRef = useRef(null);
 
-export default LineAnimation
+  useEffect(() => {
+    if (textRef.current) {
+      // Split the text into characters using SplitType
+      const myText = new SplitType(textRef.current, { types: "chars" });
+
+      
+      myText?.chars.forEach((char) => {
+        char.style.transform = "translateY(115px)";
+        char.style.transition = "transform 0.5s";
+      });
+
+      // GSAP animation for each character
+      gsap.to(myText.chars, {
+        y: 0,
+        stagger: 0.05,
+        delay: 0.2,
+        duration: 0.1,
+        ease: "power3.out",
+      });
+    }
+  }, []);
+
+  return (
+    <header className="grid place-content-center h-screen">
+      <h1
+        ref={textRef}
+        className="text-7xl uppercase font-bebas-neue leading-[5.9rem]"
+        style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}
+      >
+        Advanced AI
+      </h1>
+    </header>
+  );
+};
+
+export default LineAnimation;
